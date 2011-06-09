dab8bbf6392982dabc246e17b31efe789e47a826
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-06-09 14:13:58 +0200

    Updated manifest

--- Manifest.py
+++ Manifest.py
@@ -5,5 +5,12 @@ modules =  {"local" :
 												"modules/wr_softpll",
 												"modules/wrc_lm32",
 												"modules/wrsw_endpoint",
-												"modules/wrsw_pps_gen" ]}
+												"modules/wrsw_pps_gen",
+												"modules/wrc_core" ],
+												
+							"git" : "git://ohwr.org/hdl-core-lib/general-cores.git" }
+							
+fetchto="ip_cores"
+												
+
 												
