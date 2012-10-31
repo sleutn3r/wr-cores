67311a89986a89fee833fcd02ce563ed7fb85d37
Maciej Lipinski <maciej.lipinski@cern.ch>
2012-10-31 10:11:42 +0100

    adding platform to the main Manifest

--- Manifest.py
+++ Manifest.py
@@ -9,6 +9,7 @@ modules =  {"local" :
 												"modules/wr_endpoint",
 												"modules/wr_pps_gen",
 												"modules/wr_dacs",
+												"platform",
 												"modules/wrc_core" ],
                       "git" : "git://ohwr.org/hdl-core-lib/general-cores.git"
 						}
