7364123bbfce41b3decaa0ed346bee09da060957
Cesar Prados <c.prados@gsi.de>
2013-02-12 14:48:45 +0100

    gsi_vetar:project files for the vme gsi card

---
+++ syn/gsi_vetar/wr_core_demo/Manifest.py
@@ -0,0 +1,14 @@
+target = "altera"
+action = "synthesis"
+
+fetchto = "../../../ip_cores"
+
+syn_device = "ep2agx125ef"
+syn_grade = "c5"
+syn_package = "25"
+syn_top = "vetar_top"
+syn_project = "vetar_top"
+
+modules = {"local" : [ "../../../", "../../../top/gsi_vetar/wr_core_demo"]}
+
+				 
