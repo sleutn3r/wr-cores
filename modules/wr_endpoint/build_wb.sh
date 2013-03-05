9af5445263f0e77377cb82f208e6e19e82e79579
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2013-03-05 15:39:09 +0100

    greg_counters: never use your private path in build_wb

--- modules/wr_endpoint/build_wb.sh
+++ modules/wr_endpoint/build_wb.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 mkdir -p doc
-wbgen2 -D ./doc/wrsw_endpoint.html ep_wishbone_controller.wb
-~/Development/repos/wishbone-gen-git/wbgen2 -C endpoint_regs.h -p ep_registers_pkg.vhd -H record -V ep_wishbone_controller.vhd  --cstyle struct --lang vhdl -K ../../sim/endpoint_regs.v ep_wishbone_controller.wb
-wbgen2 -D ./doc/wrsw_endpoint_mdio.html pcs_regs.wb
-~/Development/repos/wishbone-gen-git/wbgen2 -V ep_pcs_tbi_mdio_wb.vhd --cstyle defines --lang vhdl -K ../../sim/endpoint_mdio.v pcs_regs.wb
+wbgen2 -D ./doc/wrsw_endpoint.html -C endpoint_regs.h -p ep_registers_pkg.vhd -H record -V ep_wishbone_controller.vhd  --cstyle struct --lang vhdl -K ../../sim/endpoint_regs.v ep_wishbone_controller.wb
+wbgen2 -D ./doc/wrsw_endpoint_mdio.html -V ep_pcs_tbi_mdio_wb.vhd --cstyle defines --lang vhdl -K ../../sim/endpoint_mdio.v pcs_regs.wb
